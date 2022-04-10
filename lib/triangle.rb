e778f2f285d2060d39090c16da01a6fa37653946
Jacob Yaw <yawjacob@yahoo.com>
2022-04-10 18:47:30 -0500

    commit tree 7414b8be2d550bfc5900049ac3beca200074cf83

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    validates_triangle
+    if l1 == l2 && l2 == l3
+      :equilateral
+    elsif l1 == l2 && l2 != l3
+      :isosceles
+    else l1 != l2 && l2 != l3
+      :scalene
+    end
+  end
+
+# must be larger than 0, create method that determines sides are greater than 0.
+
+# check to make sure the sum of any 2 sides is greater than the 3rd side
+
+def validates_triangle
+  raise TriangleError
 end
+
+      class TriangleError < StandardError
+      end
+  end
